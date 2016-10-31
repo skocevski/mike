72fda8a9dec01950bdca132e4a08ae767bfa6da4
skocevski <skocevski@uni-koblenz.de>
2016-10-31 11:20:30 +0100

    Update mike_assignment2.py

--- assaginment1/mike_assignment2.py
+++ assaginment1/mike_assignment2.py
@@ -3,7 +3,7 @@ import random                           #importing the library random, to use th
 
 x = 0                                   #declaring integer variable to use it as a counter later in a loop
 while (x < 10):                         #while loop that circels 10 times
-    y = random.randint(0, 99)           #declaring y variable that has the value of the result of the function randit() which generates random value in the range of 0-99
+    y = random.randint(0, 90)           #declaring y variable that has the value of the result of the function randit() which generates random value in the range of 0-99
     s = math.sin(y)                     #declaring s variable that holds the result of the function sinus() of the previous randomly generated number
     c = math.cos(y)                     #declaring c variable that holds the result of the function cosine() of the previous randomly generated number
     print("Number %d has \t sin:%f \t  and cosine:%f" % (y, s, c))  #command print to show the randoly generated number and its sinus in cosinus
